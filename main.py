565245d3fc4dd4328574088010d4785e0d2bbb0c
Arun Kulshreshtha <kulshrax@gmail.com>
2015-11-15 22:12:04 -0800

    Got a basic graph model working.

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-"""
-Example code for working with the database.
-
-psycopg2 documentation: http://initd.org/psycopg/docs/index.html
-
-"""
-
 import psycopg2
 import snap
 import sys
@@ -31,18 +24,56 @@ def results(cursor):
         yield result
 
 
+def add_nodes(cur, graph):
+    """Add users to graph as nodes."""
+    cur.execute("SELECT id FROM se_user;")
+    for row in results(cur):
+        user_id = row[0]
+
+        # Filter out dummy users with ID < 0.
+        if user_id < 0:
+            continue
+        graph.AddNode(user_id)
+
+
+def add_edges(cur, graph):
+    """Add a directed edge between each pair of nodes where the source
+       user answered a question asked by the destination user.
+    """
+
+    query = """SELECT DISTINCT t1.owner_user_id, t2.owner_user_id
+               FROM Post t1
+               INNER JOIN Post t2
+               ON t1.parent_id = t2.id
+               WHERE t1.post_type_id = 2 and t2.post_type_id = 1;
+            """
+
+    cur.execute(query)
+    for src, dst in results(cur):
+        if src is None or dst is None:
+            continue
+        graph.AddEdge(src, dst)
+
+
+def build_graph(cur):
+    graph = snap.TNGraph.New()
+    add_nodes(cur, graph)
+    add_edges(cur, graph)
+    return graph
+
+
 def main(argv):
-    if len(argv) < 2:
-        print "Usage: {} limit".format(argv[0])
-        return
+    db_name = DB_NAME
+    db_user = DB_USER
+    if len(argv) > 3:
+        db_name = argv[1]
+        db_user = argv[2]
 
-    limit = argv[1]
-
-    conn, cur = connect()
-    cur.execute("SELECT * FROM Post LIMIT %s", (limit,))
-
-    for result in results(cur):
-        print result
+    conn, cur = connect(db_name, db_user)
+    graph = build_graph(cur)
+    
+    print "Nodes in graph:", graph.GetNodes()
+    print "Edges in graph:", graph.GetEdges()
 
 
 if __name__ == '__main__':
