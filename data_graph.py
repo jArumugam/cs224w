b753331c6bc6121987c8c2c9cf3faa2a5b8344a4
Arun Kulshreshtha <kulshrax@gmail.com>
2015-11-15 22:21:16 -0800

    Renamed script.

---
+++ data_graph.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import psycopg2
+import snap
+import sys
+
+DB_NAME = "kulshrax"
+DB_USER = "kulshrax"
+
+
+def connect(db=DB_NAME, user=DB_USER):
+    """Connect to the specified Postgres database as the specified user."""
+    conn = psycopg2.connect("dbname={} user={}".format(db, user))
+    cur = conn.cursor()
+    return conn, cur
+
+
+def results(cursor):
+    """Generator for iterating over query results one at a time."""
+    while True:
+        result = cursor.fetchone()
+        if result is None:
+            break
+        yield result
+
+
+def add_nodes(cur, graph):
+    """Add users to graph as nodes."""
+    cur.execute("SELECT id FROM se_user;")
+    for row in results(cur):
+        user_id = row[0]
+
+        # Filter out dummy users with ID < 0.
+        if user_id < 0:
+            continue
+        graph.AddNode(user_id)
+
+
+def add_edges(cur, graph):
+    """Add a directed edge between each pair of nodes where the source
+       user answered a question asked by the destination user.
+    """
+
+    query = """SELECT DISTINCT t1.owner_user_id, t2.owner_user_id
+               FROM Post t1
+               INNER JOIN Post t2
+               ON t1.parent_id = t2.id
+               WHERE t1.post_type_id = 2 and t2.post_type_id = 1;
+            """
+
+    cur.execute(query)
+    for src, dst in results(cur):
+        if src is None or dst is None:
+            continue
+        graph.AddEdge(src, dst)
+
+
+def build_graph(cur):
+    graph = snap.TNGraph.New()
+    add_nodes(cur, graph)
+    add_edges(cur, graph)
+    return graph
+
+
+def main(argv):
+    db_name = DB_NAME
+    db_user = DB_USER
+    if len(argv) > 3:
+        db_name = argv[1]
+        db_user = argv[2]
+
+    conn, cur = connect(db_name, db_user)
+    graph = build_graph(cur)
+    
+    print "Nodes in graph:", graph.GetNodes()
+    print "Edges in graph:", graph.GetEdges()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
