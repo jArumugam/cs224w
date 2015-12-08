9cde07b432088160b55d708544e35e2d61443d41
bhnascar <bhnascar@gmail.com>
2015-12-08 03:12:44 -0800

    Feature extraction

---
+++ feature_extractor.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+"""
+Extracts graph features for PCA and logistic regression.
+For each user, we we generate a training example with:
+
+label - expert or nonexpert
+[0] Authority score at 10th percentile of active lifetime
+[1] Authority score at 20th percentile of active lifetime
+[2] Authority score at 30th percentile of active lifetime
+[3] Authority score at 40th percentile of active lifetime
+[4] Authority score at 50th percentile of active lifetime
+[5] PageRank score at 10th percentile of active lifetime
+[6] PageRank score at 20th percentile of active lifetime
+[7] PageRank score at 30th percentile of active lifetime
+[8] PageRank score at 40th percentile of active lifetime
+[9] PageRank score at 50th percentile of active lifetime
+[10] ELO score at 10th percentile of active lifetime
+[11] ELO score at 20th percentile of active lifetime
+[12] ELO score at 30th percentile of active lifetime
+[13] ELO score at 40th percentile of active lifetime
+[14] ELO score at 50th percentile of active lifetime
+[15] CAU score at 10th percentile of active lifetime
+[16] CAU score at 20th percentile of active lifetime
+[17] CAU score at 30th percentile of active lifetime
+[18] CAU score at 40th percentile of active lifetime
+[19] CAU score at 50th percentile of active lifetime
+
+Our goal is to use these evolution of their network
+properties at early stages to predict where they will
+be at the end of the dataset (expert or nonexpert).
+
+Because it is difficult to make a relation between
+experts as identified by StackExchange reputation
+and experts as vetted by people in real life, for the
+purpose of this project we will assume that reputation
+is ground truth for expertise. We define experts to be
+the top 10 percentile of users by StackExchange reputation.
+"""
+import sys
+import metrics
+import search_utilities
+
+feature_percentiles = [.1, .2, .3, .4, .5]
+
+def cau_scores(cur, conn, user_id):
+    """
+    Returns a user CAU scores from 10th to 50th percentile.
+    """
+    return metrics.cau_for_user(cur, conn, user_id, samples = feature_percentiles)
+
+def elo_scores(cur, conn, user_id):
+    """
+    Returns a user ELO scores from 10th to 50th percentile.
+    """
+    return metrics.elo_for_user(cur, conn, user_id, samples = feature_percentiles)
+
+def pagerank_scores(cur, user_id):
+    """
+    Returns a user PageRank scores from 10th to 50th percentile.
+    """
+    return metrics.pagerank_for_user(cur, user_id, samples = feature_percentiles)
+
+def auth_scores(cur, user_id):
+    """
+    Returns a user Auth scores from 10th to 50th percentile.
+    """
+    return metrics.auth_for_user(cur, user_id, samples = feature_percentiles)
+
+def is_expert(user_id):
+    """
+    Returns true if the user is an expert.
+    """
+    return user_id in search_utilities.get_experts()
+
+def training_examples(cur, conn, user_ids):
+    """
+    Returns a set of feature vectors and labels extracted
+    from the dataset for the given user_ids.
+    """
+    fv = []
+    labels = []
+    for user_id in user_ids:
+        uv = []
+        uv += auth_scores(cur, user_id)
+        uv += pagerank_scores(cur, user_id)
+        uv += elo_scores(cur, conn, user_id)
+        uv += cau_scores(cur, conn, user_id)
+        fv.append(uv)
+        labels.append(1 if is_expert(user_id) else 0)
+    return fv, labels
+
+def main(args):
+    conn, cur = metrics.connect("Ben-han", "Ben-han")
+    user_ids = search_utilities.get_experts()
+    print user_ids
+    print training_examples(cur, conn, user_ids)
+
+if __name__ == '__main__':
+    main(sys.argv)
--- metrics.py
+++ metrics.py
@@ -33,14 +33,14 @@ def get_start_time(userID, cur):
     cur.execute(query, {'id': userID})
     return [i[0] for i in results(cur)][0]
 
-def percentile_normalization(userID, cur):
+def percentile_normalization(userID, cur, sampling_percentiles):
     """returns a vector of end times for percentiles 0, 10, 20,...100. This end time should be used inclusively"""
     times = []
     query1 = "select creation_date from post where owner_user_id = %(id)s and (post_type_id = 1 or post_type_id = 2) order by creation_date"
     cur.execute(query1, {'id': userID})
     posts = [i[0] for i in results(cur)]
     start = get_start_time(userID, cur)
-    for p in percentiles:
+    for p in sampling_percentiles:
         x = int(len(posts) * p)
         if x == 0:
             times.append(start)
@@ -54,18 +54,12 @@ def total_answers_helper(cur, start_time, end_time, userID):
     return int([i[0] for i in results(cur)][0])
 
 def total_answers(userID, cur):
-    times = percentile_normalization(userID, cur)
+    times = percentile_normalization(userID, cur, percentiles)
     result = []
     start_time = get_start_time(userID, cur)
     for time in times:
         result.append(total_answers_helper(cur, start_time, time, userID))
     return result
-
-def get_cau_at_time(cur, conn, userID, time):
-    return cau.cau(cur, conn, userID, time)
-
-def get_elo_at_time(cur, conn, userID, time):
-    return elo.elo(cur, conn, userID, time)
 
 def total_accepted_answers_helper(cur, start_time, end_time, userID):
     query = "select count(x.id) from post x, post y where y.accepted_answer_id = x.id and x.owner_user_id = %(id)s and x.post_type_id = 2 and x.creation_date >= %(start_time)s and x.creation_date <= %(end_time)s"
@@ -73,7 +67,7 @@ def total_accepted_answers_helper(cur, start_time, end_time, userID):
     return int([i[0] for i in results(cur)][0])
 
 def total_accepted_answers(userID, cur):
-    times = percentile_normalization(userID, cur)
+    times = percentile_normalization(userID, cur, percentiles)
     result = []
     start_time = get_start_time(userID, cur)
     for time in times:
@@ -104,30 +98,50 @@ def get_auth_at_time(cur, userID, time):
     ranks = graph2.hits(graph)
     return ranks[userID][1]
 
-def cau_for_user(cur, conn, userID):
-    times = percentile_normalization(userID, cur)
+def get_cau_at_time(cur, conn, userID, time):
+    return cau.cau(cur, conn, userID, time)
+
+def get_elo_at_time(cur, conn, userID, time):
+    return elo.elo(cur, conn, userID, time)
+
+def cau_for_user(cur, conn, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_cau_at_time(cur, conn, userID, t) for t in times]
 
-def elo_for_user(cur, conn, userID):
-    times = percentile_normalization(userID, cur)
+def elo_for_user(cur, conn, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_elo_at_time(cur, conn, userID, t) for t in times]
 
-def pagerank_for_user(cur, userID):
-    times = percentile_normalization(userID, cur)
+def pagerank_for_user(cur, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_pagerank_at_time(cur, userID, t) for t in times]
 
-def auth_for_user(cur, userID):
-    times = percentile_normalization(userID, cur)
+def auth_for_user(cur, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_auth_at_time(cur, userID, t) for t in times]
 
-def indegree_for_user(cur, userID):
-    times = percentile_normalization(userID, cur)
+def indegree_for_user(cur, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_indegree_at_time(cur, userID, t) for t in times]
 
-def betweenness_for_user(cur, userID):
-    times = percentile_normalization(userID, cur)
+def betweenness_for_user(cur, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_betweenness_at_time(cur, userID, t) for t in times]
 
-def closeness_for_user(cur, userID):
-    times = percentile_normalization(userID, cur)
+def closeness_for_user(cur, userID, samples = None):
+    if not samples:
+        samples = percentiles
+    times = percentile_normalization(userID, cur, samples)
     return [get_closeness_at_time(cur, userID, t) for t in times]
